21a224d1c0186c5a4d01acca16469d0c4757191d
Trần Thanh Tịnh <thanhtinhpas1@gmail.com>
2019-04-19 11:43:14 +0700

    develop

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var config = require('config');
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//static folder
+app.use(express.static(__dirname + "/public"));
+
+//set up engine 
+app.set("views", __dirname + "/apps/views")
+app.set("view engine", "ejs");
+
+var controllers = require(__dirname + "/apps/controllers");
+app.use(controllers);
+
+var host = config.get("server.host");
+var port = config.get("server.port");
+
+app.listen(port, host, function(){
+    console.log("Server is running on port: ", port);
+});
