0d4fb17282df9ecf8a8d9102a797d51631b65f8e
Trần Thanh Tịnh <thanhtinhpas1@gmail.com>
2019-04-22 00:14:22 +0700

    aaa

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(controllers);
 
 var port = config.get("server.port");
 
-app.listen(port || 3000, function(){
+app.listen(process.env.PORT || port, function(){
     console.log("Server is running on port: ", port);
 });
