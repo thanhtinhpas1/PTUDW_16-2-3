0c267bc192aa29172948bfca6c041fa7c0ea6f3f
Trần Thanh Tịnh <thanhtinhpas1@gmail.com>
2019-04-23 00:02:13 +0700

    create_base-view

--- apps/controllers/archieve-post.js
+++ apps/controllers/archieve-post.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("02_archive-page");
+    res.render("02_archive-page", {title:"archieve-post",layout: "base-view"});
 });
 
 module.exports = router;
--- apps/controllers/single-post.js
+++ apps/controllers/single-post.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("03_single-post");
+    res.render("03_single-post", {title: "single-post", layout:'base-view'});
 });
 
 module.exports = router;
