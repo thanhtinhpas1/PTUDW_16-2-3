f92a8442e5b32ed52188c0ca477deb5ae626ce44
= <=>
2019-05-07 22:59:24 +0700

    add database into base_view

--- apps/controllers/archieve-post.js
+++ apps/controllers/archieve-post.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var newdb = require("../models/news");
+
 
 router.get("/", function(req, res) {
-    res.render("02_archive-page", {title:"archieve-post",layout: "base-view"});
+    var newDatabase = newdb.loadNews();
+    newDatabase.then(rows => {
+        
+        var list = rows;
+        console.log(list);
+        res.render("02_archive-page", {
+            title:"archieve-post",
+            layout: "base-view",
+            listNew: rows
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+   
 });
 
 module.exports = router;
